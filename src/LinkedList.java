d095c22d4bd2cf98454f41b4dc67457ef8e14373
Leonardo <leonardocastrillongiraldo@gmail.com>
2024-04-18 20:55:47 -0500

    LinkList remove, reverse methods

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -28,8 +28,20 @@ public class LinkedList extends Node {
         return tail;
     }
 
-    public void removeFirst(){
+    public Node removeFirst(){
+        return new Node();
+    }
 
+    public Node removeLast(){
+        Node temp;
+        Node prev;
+
+        prev = get(length - 2);
+        temp = prev.next;
+        temp.next = null;
+        prev.next = null;
+
+        return temp;
     }
 
     public Node get(int index){
@@ -40,5 +52,60 @@ public class LinkedList extends Node {
             temp = temp.next;
         }
         return temp;
+    }
+    public boolean set(int index, int value){
+        Node temp = get(index);
+        if(temp != null){
+            temp.value = value;
+            return true;
+        }
+        return false;
+
+    }
+    public boolean insert(int index, int value){
+        if (index < 0 || index > length) return false;
+
+        if (index == 0){
+            //prepend(value);
+            return true;
+        }
+        if(index == length){
+            //append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return false;
+    }
+
+    public Node remove(int index){
+        if(index  < 0 || index >= length) return null;
+        if(index == 0 ) return removeFirst();
+        if(index == length - 1) return removeLast();
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length --;
+        return temp;
+    }
+
+    public void reverse(){
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
     }
 }
